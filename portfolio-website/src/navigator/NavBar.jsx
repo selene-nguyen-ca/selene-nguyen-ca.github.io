9d218f2bdb932f82cddd1acd2929355df3714683
AlexNgGit <nguyenphuocdat0976@gmail.com>
2026-08-23 21:13:00 -0700

    flex-style on mobile

--- portfolio-website/src/navigator/NavBar.jsx
+++ portfolio-website/src/navigator/NavBar.jsx
@@ -3,6 +3,7 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import MenuIcon from "@mui/icons-material/Menu";
 
 import { Link } from "react-router-dom";
 
@@ -21,6 +22,7 @@ function Navbar() {
           display: "flex",
           justifyContent: "space-between",
           px: { xs: 2, md: 6 },
+          minHeight: { xs: 64, md: 72 },
         }}
       >
         {/* Logo / Name */}
@@ -32,51 +34,55 @@ function Navbar() {
             color: "inherit",
             textDecoration: "none",
             fontWeight: 700,
+            fontSize: { xs: "1rem", md: "1.25rem" },
           }}
         >
           Selene Nguyen
         </Typography>
 
-        {/* Navigation Links */}
-        <Box sx={{ display: "flex", gap: 1 }}>
-          <Button
-            component={Link}
-            to="/"
-            sx={{ color: "inherit" }}
-          >
+        {/* Desktop Navigation */}
+        <Box
+          sx={{
+            display: { xs: "none", md: "flex" },
+            alignItems: "center",
+            gap: 1,
+          }}
+        >
+          <Button component={Link} to="/" sx={{ color: "inherit" }}>
             Home
           </Button>
 
-          <Button
-            component={Link}
-            to="/projects"
-            sx={{ color: "inherit" }}
-          >
+          <Button component={Link} to="/projects" sx={{ color: "inherit" }}>
             Projects
           </Button>
 
-          <Button
-            component={Link}
-            to="/resume"
-            sx={{ color: "inherit" }}
-          >
+          <Button component={Link} to="/resume" sx={{ color: "inherit" }}>
             Resume
-          </Button>
-          <Button
-          component={Link}
-            to="/mundane-blog"
-            sx={{ color: "inherit" }}>
-            Every Letter I sent you
           </Button>
 
           <Button
             component={Link}
-            to="/contact"
+            to="/mundane-blog"
             sx={{ color: "inherit" }}
           >
+            Every Letter I sent you
+          </Button>
+
+          <Button component={Link} to="/contact" sx={{ color: "inherit" }}>
             Contact
           </Button>
         </Box>
+
+        {/* Mobile Menu */}
+        <Button
+          sx={{
+            display: { xs: "flex", md: "none" },
+            minWidth: "auto",
+            color: "inherit",
+          }}
+        >
+          <MenuIcon />
+        </Button>
       </Toolbar>
     </AppBar>
   );
